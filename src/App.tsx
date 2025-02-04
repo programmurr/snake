210aa5a2c03b648f516c57c219880a2ac52894f0
Conor Murray <64764695+programmurr@users.noreply.github.com>
2025-02-04 21:46:48 +0000

    Project init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.css";
+
+// Configure to import/export alphbetically
+// Generate grid from BOUNDARIES
+// Generate snake on START
+// Move snake around grid
+
+function App() {
+  const [snake, setSnake] = useState({ length: 1 });
+
+  return <div></div>;
+}
+
+export default App;
---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+const START = { x: 25, y: 25 };
+const BOUNDARIES = { x: 100, y: 100 };
+
+export { START, BOUNDARIES };
